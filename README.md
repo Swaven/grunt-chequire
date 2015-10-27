6b23421281618ac4671cc29046914d0c72d5d265
Antoine Goutagny <antoine@swaven.com>
2015-10-27 15:06:12 +0100

    Chequire grunt task

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # grunt-chequire
-Grunt plugin to check require directives point to valid files case-sensitively
+
+> Ensures require directives point to existing files (case-sensitively).
+
+## Getting Started
+This plugin requires Grunt `~0.4.5`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-chequire --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-chequire');
+```
+
+## The "chequire" task
+
+### Overview
+In your project's Gruntfile, add a section named `chequire` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  chequire: {
+    options: {
+      // Task-specific options go here.
+    },
+    your_target: {
+      // Target-specific file lists and/or options go here.
+    },
+  },
+});
+```
+
+### Options
+
+No configuration is available.
+
+### Usage Examples
+
+```js
+grunt.initConfig({
+  chequire: {
+    srv: ['app/**/*.js']
+  }
+});
+```
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
---
+++ tasks/chequire.js
@@ -0,0 +1,138 @@
+/*
+ * grunt-chequire
+ * https://github.com/Swaven/grunt-chequire
+ *
+ * Copyright (c) 2015 Swaven
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt){
+
+  grunt.registerMultiTask('chequire', 'Ensures require directives point to existing files (case-sensitively).', function() {
+
+    var path = require('path'),
+    fs = require('fs'),
+    EventEmitter = require('events').EventEmitter,
+    done = this.async(),
+    wait = new EventEmitter(),
+    rx = /require\(?\s*['"](\..+?)['"]\)?/g, // regex to detect require directives
+    pathList = [], // list of paths extracted from require directives
+    sourceList = [], // mapping to find which source file contains a problematic require
+    res = null;
+
+
+    wait.on('setup', function(total){
+      wait.totalRequires = total;
+      wait.fileCounter = 0;
+      grunt.verbose.writeln("require paths found: " + wait.totalRequires);
+    });
+
+    wait.on('fileDone', function(file){
+      grunt.verbose.writeln("exists: " + file);
+      wait.fileCounter += 1;
+      if (wait.fileCounter === wait.totalRequires){
+        grunt.log.writeln('All requires OK');
+        done(true);
+      }
+    });
+
+    // maps error to the file that references it
+    function showErr(fileName){
+      for (var i = 0; i< sourceList.length; i++){
+        var o = sourceList[i],
+        idx = o.paths.indexOf(fileName);
+
+        if (idx > -1){
+          var asis = o.asis[idx];
+          grunt.log.error("Path error in " + o.source + '\n  at: ' + asis );
+        }
+      }
+    }
+
+    this.filesSrc.forEach(function(filePath){
+      // Warn on and remove invalid source files (if nonull was set).
+      if (!grunt.file.exists(filePath)) {
+        grunt.log.warn('Source file "' + filePath + '" not found.');
+        return;
+      }
+
+      var content = grunt.file.read(filePath),
+      o = {source: filePath, paths: [], asis: []},
+      baseDir = path.dirname(filePath);
+
+      grunt.verbose.writeln("Reading " + filePath);
+
+      // Extracts all path-based require directives
+      do {
+        res = rx.exec(content);
+        if (res != null) {
+          grunt.verbose.writeln('  ' + res[0]);
+          var absPath = path.resolve(baseDir, res[1]);
+          pathList.push(absPath);
+          o.paths.push(absPath);
+          o.asis.push(res[0]);
+        }
+      }
+      while (res != null);
+
+      // each item in sourceLis is an object with these properties:
+      // - source: path to the file read
+      // - paths: absolute paths of all require in the file
+      // - asis: require directive extracted from the file
+      if (o.paths.length > 0){
+        sourceList.push(o);
+      }
+    });
+
+    // remove duplicates
+    pathList = pathList.filter(function(elem, pos){
+      return pathList.indexOf(elem) === pos;
+    });
+
+    wait.emit('setup', pathList.length);
+
+    // print map structure
+    // for (var i = 0; i< sourceList.length; i++){
+    //   var o = sourceList[i];
+    //   grunt.log.writeln(o.source);
+    //   for (var j = 0; j< o.paths.length; j++){
+    //     grunt.log.writeln('  ' + o.paths[j]);
+    //   }
+    // }
+    // grunt.log.writeln('');
+
+    function checkFileCase(filePath){
+      var name = path.basename(filePath);
+      fs.readdir(exactDir, function(err, files){
+        if (err){
+          grunt.log.error(err);
+        }
+        else if (files.indexOf(name) === -1){
+          showErr(filePath);
+        }
+        else{
+          wait.emit('fileDone', filePath);
+        }
+      });
+    }
+
+    // check each file
+    for (var i=0; i< pathList.length; i++){
+
+      var filepath = pathList[i],
+      dirName = path.dirname(filepath);
+
+      process.chdir(dirName);
+      var exactDir = process.cwd(); // get path with correct casing
+
+      if (dirName !== exactDir){ // check path against exact casing
+        showErr(filepath);
+      }
+      else {
+        checkFileCase(filepath);
+      }
+    }
+  });
+};
